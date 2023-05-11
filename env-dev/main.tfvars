d2e4aa0b90b330a1bb2eee698ac9a7b86802fb6f
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-11 17:16:38 +0530

    addes

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  {name = "dev.frontend.catalogue_url", value= "http://catalogue-dev.priyavenkat.online:8080/"}
+  {name = "dev.frontend.cart_url", value= "http://cart-dev.priyavenkat.online:8080/"}
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type        = "SecureString"
-  value = "hello world"
-  key_id = "5097161c-b303-4b58-929a-420ee212b9ca"
+resource "aws_ssm_parameter" "parameters" {
+  count =  length(var.parameters)
+  name   =  var.parameters[count.index].name
+  type   =  "String"
+  value  =  var.parameters[count.index].value
+  key_id =  "5097161c-b303-4b58-929a-420ee212b9ca"
 }
+
